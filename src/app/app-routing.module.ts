bb9392efdc1c4ba5a8e2bbc7a976d71754073ad4
ushansankalpa <ushansan99@gmail.com>
2023-12-11 11:20:19 +0530

    added router link login and register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./pages/home/home.component";
+import {LoginComponent} from "./pages/auth/login/login.component";
+import {RegisterComponent} from "./pages/auth/register/register.component";
 
 const routes: Routes = [
 
   {
     path:'home',
     component: HomeComponent
+  },
+  {
+    path:'auth/login',
+    component: LoginComponent
+  },
+  {
+    path:'auth/register',
+    component: RegisterComponent
   },
   {
     path: '',
