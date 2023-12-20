99adc176d602c7d42dd4df13f1628de07cfb7826
sanjulaonline <sanjula692@gmail.com>
2023-12-19 21:55:08 -1200

    added: model

--- src/app/pages/blockchaindev/art-model/art-model.component.ts
+++ src/app/pages/blockchaindev/art-model/art-model.component.ts
@@ -9,24 +9,34 @@ import { ThemeService } from 'src/app/_Services/theme/theme.service';
   styleUrls: ['./art-model.component.scss']
 })
 
-export class ArtModelComponent implements OnInit {
-  @ViewChild('canvas3d', { static: true }) canvasRef!: ElementRef<HTMLCanvasElement>;
+export class ArtModelComponent  {
+  @ViewChild('canvas3d', { static: true }) canvasRef: ElementRef<HTMLCanvasElement>;
 
-  constructor(private themeService: ThemeService) { }
-
-  ngOnInit(): void {
-    this.themeService.currentTheme.subscribe(theme => {
-      const canvas: HTMLCanvasElement = this.canvasRef.nativeElement;
-      const app: Application = new Application(canvas);
-      if (theme === 'light') {
-        app.load('https://prod.spline.design/6p4zPYN51A3bC2HL/scene.splinecode');
-      } else {
-        app.load('https://prod.spline.design/y27l7kAcAI9mrGzi/scene.splinecode');
-      }
-    });
+  ngAfterViewInit() {
+    const canvas = this.canvasRef.nativeElement;
+    const app = new Application(canvas);
+    app.load('https://prod.spline.design/6p4zPYN51A3bC2HL/scene.splinecode');
   }
+}
 
-  switchTheme(theme: string) {
-    this.themeService.switchTheme(theme);
-  }
- }
+// export class ArtModelComponent implements OnInit {
+//   @ViewChild('canvas3d', { static: true }) canvasRef!: ElementRef<HTMLCanvasElement>;
+
+//   constructor(private themeService: ThemeService) { }
+
+//   ngOnInit(): void {
+//     this.themeService.currentTheme.subscribe(theme => {
+//       const canvas: HTMLCanvasElement = this.canvasRef.nativeElement;
+//       const app: Application = new Application(canvas);
+//       if (theme === 'light') {
+//         app.load('https://prod.spline.design/6p4zPYN51A3bC2HL/scene.splinecode');
+//       } else {
+//         app.load('https://prod.spline.design/6p4zPYN51A3bC2HL/scene.splinecode');
+//       }
+//     });
+//   }
+
+//   switchTheme(theme: string) {
+//     this.themeService.switchTheme(theme);
+//   }
+//  }
