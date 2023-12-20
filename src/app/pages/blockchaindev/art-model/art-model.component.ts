73f2d879e491264583753285d1735fa6b3c8f121
sanjulaonline <sanjula692@gmail.com>
2023-12-19 21:43:54 -1200

    added: model for blockchain page

--- src/app/pages/blockchaindev/art-model/art-model.component.ts
+++ src/app/pages/blockchaindev/art-model/art-model.component.ts
@@ -1,11 +1,6 @@
-import { Component, ElementRef, ViewChild,AfterViewInit } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
 import { Application } from '@splinetool/runtime';
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'; // GLTFLoader for Angular
-import { OrbitControls as OrbitControlsType } from 'three/examples/jsm/controls/OrbitControls'; // Import OrbitControls typeimport { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import SplineLoader from '@splinetool/loader';
-import {ThemeService} from "../../../_Services/theme/theme.service";
+import { ThemeService } from 'src/app/_Services/theme/theme.service';
 
 
 @Component({
@@ -14,99 +9,24 @@ import {ThemeService} from "../../../_Services/theme/theme.service";
   styleUrls: ['./art-model.component.scss']
 })
 
-export class ArtModelComponent implements AfterViewInit {
-  // @ViewChild('rendererContainer', { static: true }) rendererContainer: ElementRef<HTMLDivElement>;
+export class ArtModelComponent implements OnInit {
+  @ViewChild('canvas3d', { static: true }) canvasRef!: ElementRef<HTMLCanvasElement>;
 
-  // private scene: THREE.Scene;
-  // private camera: THREE.PerspectiveCamera;
-  // private renderer: THREE.WebGLRenderer;
-  // private controls: OrbitControls;
+  constructor(private themeService: ThemeService) { }
 
-  // constructor() { }
-
-  // ngOnInit() {
-  //   this.initThree();
-  //   this.loadSpline();
-  //   this.animate();
-  //   window.addEventListener('resize', this.onWindowResize.bind(this));
-  // }
-
-  // ngOnDestroy() {
-  //   window.removeEventListener('resize', this.onWindowResize.bind(this));
-  // }
-
-  // private initThree() {
-  //   const width = window.innerWidth;
-  //   const height = window.innerHeight;
-
-  //   this.scene = new THREE.Scene();
-
-  //   this.camera = new THREE.PerspectiveCamera(45, width / height, 70, 100000);
-  //   this.camera.position.set(675.76, 178.47, 227.72);
-  //   this.camera.quaternion.setFromEuler(new THREE.Euler(-0.66, 1.17, 0.62));
-
-  //   this.renderer = new THREE.WebGLRenderer({ antialias: true });
-  //   this.renderer.setSize(width, height);
-  //   this.renderer.shadowMap.enabled = true;
-  //   this.renderer.shadowMap.type = THREE.PCFShadowMap;
-  //   this.renderer.setClearColor('#ffff', 1);
-
-  //   this.renderer.domElement.style.display = 'block';
-  //   this.rendererContainer.nativeElement.appendChild(this.renderer.domElement);
-
-  //   this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-  //   this.controls.enableDamping = true;
-  //   this.controls.dampingFactor = 0.125;
-  // }
-
-  // private loadSpline() {
-  //   const loader = new SplineLoader();
-  //   loader.load(
-  //     'https://prod.spline.design/y27l7kAcAI9mrGzi/scene.splinecode',
-  //     (splineScene) => {
-  //       this.scene.add(splineScene);
-  //     }
-  //   );
-  // }
-
-  // private onWindowResize() {
-  //   const width = window.innerWidth;
-  //   const height = window.innerHeight;
-
-  //   this.camera.aspect = width / height;
-  //   this.camera.updateProjectionMatrix();
-  //   this.renderer.setSize(width, height);
-  // }
-
-  // private animate() {
-  //   const animateFn = () => {
-  //     requestAnimationFrame(animateFn);
-  //     this.controls.update();
-  //     this.renderer.render(this.scene, this.camera);
-  //   };
-  //   animateFn();
-  // }
-
-  constructor(private elementRef: ElementRef, private themeService: ThemeService) {
+  ngOnInit(): void {
+    this.themeService.currentTheme.subscribe(theme => {
+      const canvas: HTMLCanvasElement = this.canvasRef.nativeElement;
+      const app: Application = new Application(canvas);
+      if (theme === 'light') {
+        app.load('https://prod.spline.design/6p4zPYN51A3bC2HL/scene.splinecode');
+      } else {
+        app.load('https://prod.spline.design/y27l7kAcAI9mrGzi/scene.splinecode');
+      }
+    });
   }
-  @ViewChild('canvas3d', { static: true }) canvasRef: ElementRef<HTMLCanvasElement>;
 
-  @ViewChild('canvas3dDark', { static: true }) canvasRefDark: ElementRef<HTMLCanvasElement>;
-
-  ngAfterViewInit() {
-    let mode = this.themeService.isDarkMode();
-    const canvas = this.canvasRef.nativeElement;
-    const canvasDark = this.canvasRefDark.nativeElement;
-    const app = new Application(canvas);
-    const appDark = new Application(canvasDark);
-    app.load('https://prod.spline.design/g0qjQxb89G0dnY82/scene.splinecode');
-    appDark.load('https://prod.spline.design/y27l7kAcAI9mrGzi/scene.splinecode');
-    // if (!mode){
-    //   app.load('https://prod.spline.design/g0qjQxb89G0dnY82/scene.splinecode');
-    // }else {
-    //   appDark.load('https://prod.spline.design/y27l7kAcAI9mrGzi/scene.splinecode');
-    // }
-
-    // app.load('https://prod.spline.design/y27l7kAcAI9mrGzi/scene.splinecode');
+  switchTheme(theme: string) {
+    this.themeService.switchTheme(theme);
   }
-}
+ }
--- src/app/pages/blockchaindev/art-model/spline-data.model.ts
+++
@@ -1,5 +0,0 @@
-export interface SplineData {
-    nodes: any;
-    materials: any;
-  }
-  
