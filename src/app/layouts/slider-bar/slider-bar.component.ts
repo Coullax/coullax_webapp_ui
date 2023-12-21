b28504047bfccbeb630d4933437feefc8edaf921
sanjulaonline <sanjula692@gmail.com>
2023-12-20 19:03:58 -1200

    fix: slider

--- src/app/layouts/slider-bar/slider-bar.component.ts
+++ src/app/layouts/slider-bar/slider-bar.component.ts
@@ -1,24 +1,48 @@
-import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import Swiper from 'swiper';
 import GLightbox from 'glightbox';
+
 @Component({
   selector: 'app-slider-bar',
   templateUrl: './slider-bar.component.html',
   styleUrls: ['./slider-bar.component.scss'],
 })
-export class SliderBarComponent implements OnInit {
+export class SliderBarComponent implements OnInit, AfterViewInit {
 
-
+  @ViewChild('swiper', { static: false }) swiperEl!: ElementRef;
 
   lightbox: any;
+  swiper: Swiper | undefined;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(): void {
+    this.initSwiper();
+  }
 
-  playVideo(url: any){
-    debugger
+  initSwiper(): void {
+    this.swiper = new Swiper(this.swiperEl.nativeElement, {
+      slidesPerView: 'auto',
+      spaceBetween: 1,
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+        1920: {
+          slidesPerView: 2,
+        },
+      },
+    });
+  }
+
+  playVideo(url: any) {
+    debugger;
 
     this.lightbox = GLightbox({
       'href': `${url}`,
@@ -26,16 +50,15 @@ export class SliderBarComponent implements OnInit {
       'source': 'youtube',
       'width': 300,
       'autoplayvideo': true,
-    }),
+    });
     this.lightbox.open();
-    this.lightbox.on('open', ()=>{
+    this.lightbox.on('open', () => {
 
     });
-    this.lightbox.on('close', ()=>{
+    this.lightbox.on('close', () => {
       this.lightbox.destroy();
-    })
+    });
   }
-
 
   videoList = [
     {
@@ -47,7 +70,5 @@ export class SliderBarComponent implements OnInit {
     {
       id: 'http://www.youtube.com/watch?v=RgKAFK5djSk&ab_channel=WizKhalifaMusic'
     }
-  ]
-
-
+  ];
 }
