aeae63374a8d93163d1a1fbb3ac04043e5880f9e
ushansankalpa <ushansan99@gmail.com>
2023-12-27 12:23:46 +0530

    complete mobile view of nft page

--- src/app/pages/nft/nft.component.ts
+++ src/app/pages/nft/nft.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BreakpointObserver, BreakpointState} from "@angular/cdk/layout";
 
 @Component({
   selector: 'app-nft',
   templateUrl: './nft.component.html',
   styleUrls: ['./nft.component.scss']
 })
-export class NftComponent {
-
+export class NftComponent implements OnInit{
+  showNaveBar:boolean=true;
+  constructor(public breakpointObserver: BreakpointObserver) {
+  }
+  ngOnInit() {
+    this.breakpointObserver
+      .observe(['(min-width: 900px)'])
+      .subscribe((state: BreakpointState) => {
+        if (state.matches) {
+          this.showNaveBar = true;
+        } else {
+          this.showNaveBar = false;
+        }
+      });
+  }
 }
