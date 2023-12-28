7a90075a620b6c4b7e679df028273e452d27dd56
ushansankalpa <ushansan99@gmail.com>
2023-12-28 11:59:43 +0530

    complete uiux mobile and web page

--- src/app/pages/uiux/uiux.component.ts
+++ src/app/pages/uiux/uiux.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BreakpointObserver, BreakpointState} from "@angular/cdk/layout";
 
 @Component({
   selector: 'app-uiux',
   templateUrl: './uiux.component.html',
   styleUrls: ['./uiux.component.scss']
 })
-export class UiuxComponent {
-
+export class UiuxComponent implements OnInit{
+  showNaveBar:boolean=true;
+  constructor(public breakpointObserver: BreakpointObserver) {
+  }
+  ngOnInit() {
+    this.breakpointObserver
+      .observe(['(min-width: 900px)'])
+      .subscribe((state: BreakpointState) => {
+        if (state.matches) {
+          this.showNaveBar = true;
+        } else {
+          this.showNaveBar = false;
+        }
+      });
+  }
 }
