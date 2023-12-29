1c84977c21a362e26f0d751e8709b6f40a245a09
ushansankalpa <ushansan99@gmail.com>
2023-12-29 16:05:50 +0530

    completed home page

--- src/app/layouts/banner/banner.component.ts
+++ src/app/layouts/banner/banner.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import AOS from "aos";
+
+declare var window: any;
+
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.scss']
 })
-export class BannerComponent {
+
+
+export class BannerComponent implements OnInit{
   constructor() {
     AOS.init();
   }
+  ngOnInit(): void {
+  }
+
+
 }
