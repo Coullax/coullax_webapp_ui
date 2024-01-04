8887bbacb2065c1c41b362bfb8089813cb510157
ushansankalpa <ushansan99@gmail.com>
2024-01-04 17:30:11 +0530

    added review dialog box

--- src/app/layouts/review-bar/review-bar.component.ts
+++ src/app/layouts/review-bar/review-bar.component.ts
@@ -8,7 +8,82 @@ import Flickity from 'flickity';
 })
 export class ReviewBarComponent implements OnInit {
 
+  reviews1:any = [];
+  reviews2:any = [];
   ngOnInit(): void {
+    this.reviews1 = [
+      {
+        name:'nikodejonghe77',
+        profile:'assets/review_profile/Mark.svg',
+        ratings: "5",
+        feedback:'Very good communication and quality of work!'
+      },
+
+      {
+        name:'cristianodev',
+        profile:'assets/review_profile/Mark2.svg',
+        ratings: "5",
+        feedback:'A nice designer, he got the concept right away and he was able to give me multiple revisions until we got the concept we were looking for.'
+      },
+
+      {
+        name:'jossiecotto444',
+        profile:'assets/review_profile/Mark4.svg',
+        ratings: "5",
+        feedback:'The artist made sure I got to know the process. He approached to our communication in the best manner and was always taking my references in details. I will keep business with him because I feel confident working with people that have word, talent, high-end skills, and make it easy for first timers. I loved working with him for 4 days, everything was real easy and beautiful!'
+      }
+      ,
+
+      {
+        name:'msgeorgiarose',
+        profile:'assets/review_profile/Mark5.svg',
+        ratings: "5",
+        feedback:'The output turned out great as coullax was willing to make necessary changes to meet our needs. Would work with them again.'
+      }
+      ,
+
+      {
+        name:'jamin89',
+        profile:'assets/review_profile/Mark8.svg',
+        ratings: "5",
+        feedback:'Creating an AMAZING NFT Collection. Super prompt, great to work with...SUPERIOR results!!!'
+      }
+    ]
+
+    this.reviews2 = [
+      {
+        name:'alextima10',
+        profile:'assets/review_profile/Mark1.svg',
+        ratings: "5",
+        feedback:'Best experience I’ve had . Professional and smooth. Communication and understanding of my requests was impressive. Delivered a quality product.'
+      },
+      {
+        name:'joeyaji',
+        profile:'assets/review_profile/Mark1.svg',
+        ratings: "5",
+        feedback:'The Coullax team is very talented, creative and communicative. We had a very large project project of 8000 nfts and the team worked incredibly hard and created a very beautiful collection that will go on to sell out. We will be using them again for our next project. \n' +
+          'I recommend them very highly. Thank you Coullax team! Excited to work together again in the future.'
+      },
+      {
+        name:'zac18992',
+        profile:'assets/review_profile/Mark6.svg',
+        ratings: "5",
+        feedback:'Coullax really went into detail and, although we had a lot of requests, fulfilled them all with great patience. Awesome designs!'
+      },
+      {
+        name:'efrencreates',
+        profile:'assets/review_profile/Mark7.svg',
+        ratings: "5",
+        feedback:'very knowledgeable and professional, best I could find!'
+      },
+      {
+        name:'ryanmcgurl947',
+        profile:'assets/review_profile/Mark9.svg',
+        ratings: "5",
+        feedback:'Creating an AMAZING NFT Collection. Super prompt, great to work with...SUPERIOR results!!!'
+      },
+    ];
+
     const scrollers = document.querySelectorAll(".scroller");
 
     // If a user hasn't opted in for recuded motion, then we add the animation
@@ -80,6 +155,12 @@ export class ReviewBarComponent implements OnInit {
     flickity.on('dragStart', () => {
       isPaused = true;
     });
+  }
+
+  visible: boolean = false;
+
+  showDialog() {
+    this.visible = true;
   }
 
 }
